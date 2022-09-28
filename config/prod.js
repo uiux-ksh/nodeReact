2f1c3b51856eabeb498e167e117ed64a17e43074
seonghoon kim <pole@1024corp.com>
2022-09-28 15:10:22 +0900

    dev 제외

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURL:'mongodb+srv://uiuxksh:zneh8965@kimseonghoon.5pzjrbp.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -2,9 +2,33 @@ const mongoose = require("mongoose");
 const express = require('express');
 const app = express();
 const port = 3003;
-const MONGODB_URL ='mongodb+srv://uiuxksh:zneh8965@kimseonghoon.5pzjrbp.mongodb.net/?retryWrites=true&w=majority';
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
+const config = require("./config/key");
 
 
-mongoose.connect(MONGODB_URL).then(() => console.log('MongDb')).catch((err) => console.log(err));
-app.get('/',(req,res) => res.send('Hello Word'));
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//aplication/json
+app.use(bodyParser.json());
+
+mongoose.connect(config.mongoURL).then(() => console.log('MongDb')).catch((err) => console.log(err));
+app.get('/',(req,res) => res.send('노드문 실행'));
+
+app.post('/register',(req,res) => {
+
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+     const user = new User(req.body)
+
+     user.save((err,userInfo) => {
+          if(err) return res.json({success:false, err});
+          return res.status(200).json({
+               success:true
+          })
+     });
+})
+
 app.listen(port,() => console.log(` ${port}번 포트가 열렸습니다`));
